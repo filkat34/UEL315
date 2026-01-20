56a76404ec170838788e0fa345e4d44c21c902d5
mathildechauvet <mathildechauvet@outlook.fr>
2026-01-20 13:22:31 +0100

    ex3 req8

---
+++ semaine3/Exercice 3/req8.mongobd.js
@@ -0,0 +1,7 @@
+// Connexion à la BDD
+use("UEL315_S3");
+
+// Afficher la liste de tous les éditeurs (champ "publisher") distincts
+db.getCollection("dblp").distinct("publisher", {
+  publisher: { $exists: true, $ne: null, $ne: "" },
+});
