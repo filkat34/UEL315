5f806e9dbe95b585db8dfc33d6b04c72bd0f8706
filippos <filippos29@hotmail.com>
2026-01-20 10:08:13 +0100

    fix : request5

--- semaine3/Exercice 3/req5.mongodb.js
+++ semaine3/Exercice 3/req5.mongodb.js
@@ -2,7 +2,6 @@
 use("UEL315_S3");
 
 //Afficher le titre de toutes les publications de type « Article » depuis 2012
-db.getCollection("dblp").find(
-  { type: "Article", year: { $gte: 2012 } },
-  { title: 1, _id: 0 },
-);
+db.getCollection("dblp")
+  .find({ type: "Article", year: { $gte: 2012 } }, { title: 1, _id: 0 })
+  .toArray();
